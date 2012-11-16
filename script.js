2abdc633b429044b7beb224a1b8698340eb3ee3c
Extempl <extempl@gmail.com>
2012-11-16 10:05:18 +0200

    markup of first chart

---
+++ script.js
@@ -0,0 +1,15 @@
+var linearChart = function (container, config) {
+//	this.config = config;
+	var i, l;
+	var containerWidth = config.width + 50;
+	var containerHeight = config.height + 25;
+
+	for(i = 0, l = config.axisX.points.length; i < l; i++) {
+
+	}
+
+
+	// function that composing charts container
+
+	// function that creates charts through loop over it in config
+};
