55c34dd5f93f8a97fae3a03091bea30abec74538
Extempl <extempl@gmail.com>
2012-11-18 12:51:11 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,288 +1,316 @@
 var linearChart = function (container, config) {
-//	this.config = config;
-	var i, l, x, y;
+	this.config = config;
+	this.containerEl = document.querySelector(container);
 
-	var svgNS = 'http://www.w3.org/2000/svg';
-	var containerWidth = config.width + 65;
-	var containerHeight = config.height + 80;
+	this.svgNS = 'http://www.w3.org/2000/svg';
 
-	var axisXLength = config.axisX.points.length;
-	var axisYLeftLength = config.axisY.points.length;
-	var axisYRightLength = config.axisY.points2.length;
+	this.init();
 
-	var singleStepX = config.width / (config.axisX.points[axisXLength - 1] - config.axisX.points[0]);
+	document.querySelector('.navPanel').addEventListener('click', this.onclickNav.bind(this));
 
-	var stepX = config.width / (axisXLength - 1);
-	var stepY = config.height / (axisYLeftLength - 1);
-//	var stepY2 = config.height / (axisYRightLength - 1);
-
-	// create svg container
-	var svgEl = document.createElementNS(svgNS, 'svg');
-	svgEl.setAttribute('width', containerWidth);
-	svgEl.setAttribute('height', containerHeight);
-	var gMain = document.createElementNS(svgNS, 'g');
-	gMain.setAttribute('transform', 'translate(20,60)');
-	svgEl.appendChild(gMain);
-	var containerEl = document.querySelector(container);
-	containerEl.appendChild(svgEl);
-	containerEl = svgEl.querySelector('g');
-
-	// axisX
-	var axisXLine;
-	var axisXEl;
-	var axisXTick;
-	var axisXTickText;
-	var axisXTickLine;
-
-	axisXEl = document.createElementNS(svgNS, 'g');
-	axisXEl.classList.add('axis');
-	axisXEl.classList.add('x');
-	axisXEl.setAttribute('transform', 'translate(0,' + (config.height - 1) + ')');
-
-	axisXTickLine = document.createElementNS(svgNS, 'line');
-	axisXTickLine.setAttribute('y2', 4);
-
-	axisXTickText = document.createElementNS(svgNS, 'text');
-	axisXTickText.setAttribute('dy', 17);
-
-	x = 0;
-	for(i = 0; i < axisXLength; i++) {
-		axisXTick = document.createElementNS(svgNS, 'g');
-		axisXTick.setAttribute('transform', 'translate(' + x + ',0)');
-
-		axisXTickText = axisXTickText.cloneNode();
-		axisXTickText.textContent = config.axisX.points[i];
-
-		axisXTick.appendChild(axisXTickLine.cloneNode());
-		axisXTick.appendChild(axisXTickText);
-		axisXEl.appendChild(axisXTick);
-		x += stepX;
+	var selected;
+	for(var i = 0, l = config.data.length; i < l; i++) {
+		this.generateChart(config.data[i]);
+		if(config.data[i].selected)
+			selected = true;
 	}
-	axisXLine = document.createElementNS(svgNS,'path');
-	axisXLine.classList.add('axisLine');
-	axisXLine.setAttribute('d', 'M0,1 H' + config.width);
-	axisXEl.appendChild(axisXLine);
+	if(!selected)
+		this.selectChart(config.data[0].id);
 
-	// left axisY
-	var axisYLeftEl;
-	var axisYLeftTick;
-	var axisYLeftLine;
-	var axisYLeftText;
+	this.gMain.appendChild(this.spotsEl);
+};
 
-	var point;
+linearChart.prototype = {
+	init: function () {
+		this.createSVGContainer();
+		this.initAxisX();
+		this.initAxisYLeft();
+		this.initAxisYRight();
+		this.initBasicWrappers();
+		this.initMainLine();
+		this.initDefsElements();
+		this.initSpotElements();
+		this.initVLineElements();
+	},
+	createSVGContainer: function () {
+		var svgEl = document.createElementNS(this.svgNS, 'svg'),
+			gMain = document.createElementNS(this.svgNS, 'g');
 
-	axisYLeftEl = document.createElementNS(svgNS, 'g');
-	axisYLeftEl.classList.add('axis');
-	axisYLeftEl.classList.add('yLeft');
+		svgEl.setAttribute('width', this.config.width + 65);
+		svgEl.setAttribute('height', this.config.height + 80);
+		gMain.setAttribute('transform', 'translate(20,60)');
+		svgEl.appendChild(gMain);
+		this.containerEl.appendChild(svgEl);
 
-	axisYLeftLine = document.createElementNS(svgNS, 'line');
-	axisYLeftLine.setAttribute('x2', config.width);
-	axisYLeftLine.style.stroke = config.middleLineColor;
+		this.gMain = gMain;
+	},
+	initAxisX: function () {
+		var x, i,
+			points = this.config.axisX.points,
+			axisXLength = points.length - 1,
+			stepX = this.config.width / axisXLength,
+			axisXEl = document.createElementNS(this.svgNS, 'g'),
+			axisXTickText = document.createElementNS(this.svgNS, 'text'),
+			axisXTickLine = document.createElementNS(this.svgNS, 'line'),
+			axisXLine = document.createElementNS(this.svgNS, 'path'),
+			axisXTick;
 
-	axisYLeftText = document.createElementNS(svgNS, 'text');
-	axisYLeftText.setAttribute('y', 4);
-	axisYLeftText.setAttribute('x', -20);
-	axisYLeftText.style.fill = config.middleLineColor;
+		this.singleStepX = this.config.width / (points[axisXLength] - points[0]);
 
-	for(i = axisYLeftLength; i > 1; i--) {
-		point = config.axisY.points[i-1];
+		axisXEl.classList.add('axis');
+		axisXEl.classList.add('x');
+		axisXEl.setAttribute('transform', 'translate(0,' + (this.config.height - 1) + ')');
 
-		axisYLeftTick = document.createElementNS(svgNS, 'g');
-		axisYLeftTick.setAttribute('transform', 'translate(0,' + (config.height - point * stepY) + ')');
+		axisXTickLine.setAttribute('y2', 4);
+		axisXTickText.setAttribute('dy', 17);
 
-		axisYLeftText = axisYLeftText.cloneNode();
-		axisYLeftText.textContent = point;
+		axisXLine.classList.add('axisLine');
+		axisXLine.setAttribute('d', 'M0,1 H' + this.config.width);
 
-		axisYLeftTick.appendChild(axisYLeftLine.cloneNode());
-		axisYLeftTick.appendChild(axisYLeftText);
-		axisYLeftEl.appendChild(axisYLeftTick);
-	}
+		x = 0;
+		for (i = 0; i <= axisXLength; i++) { // TODO maybe set function init x tick
+			axisXTickText = axisXTickText.cloneNode();
+			axisXTickText.textContent = points[i];
 
-	//right axisY
-	var axisYRightEl;
-	var axisYRightTick;
+			axisXTick = document.createElementNS(this.svgNS, 'g');
+			axisXTick.setAttribute('transform', 'translate(' + x + ',0)');
 
+			axisXTick.appendChild(axisXTickLine.cloneNode());
+			axisXTick.appendChild(axisXTickText);
 
-	axisYRightEl = document.createElementNS(svgNS, 'g');
-	axisYRightEl.classList.add('axis');
-	axisYRightEl.classList.add('yRight');
-	axisYRightEl.setAttribute('transform', 'translate(' + (config.width + 10) + ',5)');
-	for (i = axisYRightLength; i > 0; i--) {
-		point = config.axisY.points2[i-1];
-
-		axisYRightTick = document.createElementNS(svgNS, 'text');
-		axisYRightTick.setAttribute('transform', 'translate(0,' + (config.height - point.val * stepY) + ')');
-		axisYRightTick.textContent = point.lbl;
-
-		axisYRightEl.appendChild(axisYRightTick);
-	}
-
-	var spotsEl = document.createElementNS(svgNS, 'g'); // move to main
-	spotsEl.classList.add('spots');
-
-	var vLines = document.createElementNS(svgNS, 'g'); //move to main
-	vLines.classList.add('vLines');
-	vLines.setAttribute('transform', 'translate(0,' + config.height + ')');
-
-	var defs = document.createElementNS(svgNS, 'defs');
-
-
-	// function that composing charts container
-
-	containerEl.appendChild(axisXEl);
-	containerEl.appendChild(axisYLeftEl);
-	containerEl.appendChild(axisYRightEl);
-	containerEl.appendChild(spotsEl);
-	containerEl.appendChild(vLines);
-	containerEl.appendChild(defs);
-
-	//==============================================================
-
-	// create path
-	var mainLine = document.createElementNS(svgNS, 'path');
-	mainLine.classList.add('mainLine');
-
-	// create defs
-	var linearGradient = document.createElementNS(svgNS, 'linearGradient');
-	linearGradient.setAttribute('x', 0);
-	linearGradient.setAttribute('y', 0);
-	linearGradient.setAttribute('x2', 0);
-	linearGradient.setAttribute('y2', 1);
-	var stopEl = document.createElementNS(svgNS, 'stop');
-	stopEl.setAttribute('stop-color', '#fff');
-	stopEl.setAttribute('offset', 0);
-	stopEl.setAttribute('stop-opacity', 0.1);
-
-	var stopEl1 = stopEl.cloneNode();
-	stopEl1.setAttribute('offset', 1);
-	stopEl1.classList.add('gradientColor');
-	stopEl1.setAttribute('stop-opacity', 0.1);
-
-	linearGradient.appendChild(stopEl);
-	linearGradient.appendChild(stopEl1);
-
-	// create spots
-	var spot = document.createElementNS(svgNS, 'g');
-	var circle = document.createElementNS(svgNS, 'circle');
-	circle.setAttribute('r', 5);
-	var hintText = document.createElementNS(svgNS, 'text');
-	hintText.setAttribute('transform', 'translate(0,-21)');
-	var hintTextX = hintText.cloneNode();
-	hintText.classList.add('percentageValue');
-	hintTextX.classList.add('yValue');
-	hintTextX.setAttribute('transform', 'translate(0,-35)');
-	var hint = document.createElementNS(svgNS, 'path');
-	hint.setAttribute('d',
-		'M0,-10 l-19,-10    h  -1 ' +
-	    'a 3,3 0 0,1 -3, -3 v -28 ' +
-	    'a 3,3 0 0,1  3, -3 h  40 ' +
-	    'a 3,3 0 0,1  3,  3 v  28 ' +
-	    'a 3,3 0 0,1 -3,  3 h  -1 z');
-	var hintBG = hint.cloneNode();
-	hintBG.classList.add('hintWhiteBG');
-	hint.classList.add('hint');
-
-	spot.appendChild(circle);
-	spot.appendChild(hintBG);
-	spot.appendChild(hint);
-	spot.appendChild(hintTextX);
-	spot.appendChild(hintText);
-
-	//create vLines
-	var vLineEl = document.createElementNS(svgNS, 'g');
-	var vLinePath = document.createElementNS(svgNS, 'path');
-	var vLineTextEl = document.createElementNS(svgNS, 'g');
-	var vLineTextBG = document.createElementNS(svgNS, 'path');
-	var vLineText = document.createElementNS(svgNS, 'text');
-
-
-	vLinePath.classList.add('vLine');
-	vLineTextEl.setAttribute('transform', 'translate(0,-7)');
-	vLineTextBG.classList.add('vLineBG');
-	vLineTextBG.setAttribute('d', 'M0,0 V-15');
-	vLineText.setAttribute('y', -4);
-
-	vLineTextEl.appendChild(vLineTextBG);
-	vLineTextEl.appendChild(vLineText);
-	vLineEl.appendChild(vLinePath);
-	vLineEl.appendChild(vLineTextEl);
-
-	// menu handler
-	document.querySelector('.navPanel').addEventListener('click', function (e) {
-		if(e.target.tagName != 'SPAN')
-			return true;
-		var el = e.target;
-		for(var i = 0, l = this.children.length; i < l; i++) {
-			this.children[i].classList.remove('current');
+			axisXEl.appendChild(axisXTick);
+			x += stepX;
 		}
-		el.parentNode.classList.add('current');
-		document.querySelector('#chartContainer').setAttribute('class', 'showChart_' + el.parentNode.getAttribute('id'));
-	});
+		axisXEl.appendChild(axisXLine);
 
+		this.gMain.appendChild(axisXEl);
+	},
+	initAxisYLeft: function () {
+		var i,
+			points = this.config.axisY.points,
+			axisYLeftLength = points.length - 1,
+			axisYLeftEl = document.createElementNS(this.svgNS, 'g'),
+			axisYLeftLine = document.createElementNS(this.svgNS, 'line'),
+			axisYLeftText = document.createElementNS(this.svgNS, 'text'),
+			axisYLeftTick;
 
-	// function that creates charts through loop over it in config
+		this.stepY = this.config.height / axisYLeftLength;
 
-	this.generateChart = function (chartConfig) {
-		var j, k;
+		axisYLeftEl.classList.add('axis');
+		axisYLeftEl.classList.add('yLeft');
 
-		var currentMainLine = mainLine.cloneNode();
+		axisYLeftLine.setAttribute('x2', this.config.width);
+		axisYLeftLine.style.stroke = this.config.middleLineColor;
+
+		axisYLeftText.setAttribute('y', 4);
+		axisYLeftText.setAttribute('x', -20);
+		axisYLeftText.style.fill = this.config.middleLineColor;
+
+		for (i = axisYLeftLength; i > 0; i--) { // TODO
+			axisYLeftTick = document.createElementNS(this.svgNS, 'g');
+			axisYLeftTick.setAttribute('transform', 'translate(0,' + (this.config.height - points[i] * this.stepY) + ')');
+
+			axisYLeftText = axisYLeftText.cloneNode();
+			axisYLeftText.textContent = points[i];
+
+			axisYLeftTick.appendChild(axisYLeftLine.cloneNode());
+			axisYLeftTick.appendChild(axisYLeftText);
+			axisYLeftEl.appendChild(axisYLeftTick);
+		}
+
+		this.gMain.appendChild(axisYLeftEl);
+	},
+	initAxisYRight: function () {
+		var axisYRightLength = this.config.axisY.points2.length,
+			axisYRightEl = document.createElementNS(this.svgNS, 'g'),
+			axisYRightTick, i, point;
+
+		axisYRightEl.classList.add('axis');
+		axisYRightEl.classList.add('yRight');
+		axisYRightEl.setAttribute('transform', 'translate(' + (this.config.width + 10) + ',5)');
+
+		for (i = axisYRightLength; i > 0; i--) {
+			point = this.config.axisY.points2[i - 1];
+
+			axisYRightTick = document.createElementNS(this.svgNS, 'text');
+			axisYRightTick.setAttribute('transform', 'translate(0,' + (this.config.height - point.val * this.stepY) + ')');
+			axisYRightTick.textContent = point.lbl;
+
+			axisYRightEl.appendChild(axisYRightTick);
+		}
+
+		this.gMain.appendChild(axisYRightEl);
+	},
+	initBasicWrappers: function () {
+		this.spotsEl = document.createElementNS(this.svgNS, 'g');
+		this.vLines = document.createElementNS(this.svgNS, 'g');
+		this.defs = document.createElementNS(this.svgNS, 'defs');
+
+		this.spotsEl.classList.add('spots');
+		this.vLines.classList.add('vLines');
+		this.vLines.setAttribute('transform', 'translate(0,' + this.config.height + ')');
+
+		// function that composing charts container
+
+		this.gMain.appendChild(this.vLines);
+		this.gMain.appendChild(this.defs);
+	},
+	initMainLine: function () {
+		this.mainLine = document.createElementNS(this.svgNS, 'path');
+		this.mainLine.classList.add('mainLine');
+	},
+	initDefsElements: function () {
+		var linearGradient = document.createElementNS(this.svgNS, 'linearGradient'),
+			stopEl = document.createElementNS(this.svgNS, 'stop'),
+			stopEl1;
+
+		linearGradient.setAttribute('x', 0);
+		linearGradient.setAttribute('y', 0);
+		linearGradient.setAttribute('x2', 0);
+		linearGradient.setAttribute('y2', 1);
+
+		stopEl.setAttribute('stop-color', '#fff');
+		stopEl.setAttribute('offset', 0);
+		stopEl.setAttribute('stop-opacity', 0.1);
+
+		stopEl1 = stopEl.cloneNode();
+		stopEl1.setAttribute('offset', 1);
+		stopEl1.classList.add('gradientColor');
+
+		linearGradient.appendChild(stopEl);
+		linearGradient.appendChild(stopEl1);
+
+		this.linearGradient = linearGradient;
+	},
+	initSpotElements: function () {
+		var spot = document.createElementNS(this.svgNS, 'g'),
+			circle = document.createElementNS(this.svgNS, 'circle'),
+			hintText = document.createElementNS(this.svgNS, 'text'),
+			hint = document.createElementNS(this.svgNS, 'path'),
+			hintTextX, hintBG;
+
+		circle.setAttribute('r', 5);
+
+		hintText.setAttribute('transform', 'translate(0,-21)');
+		hintTextX = hintText.cloneNode();
+		hintText.classList.add('percentageValue');
+		hintTextX.classList.add('yValue');
+		hintTextX.setAttribute('transform', 'translate(0,-35)');
+
+		hint.setAttribute('d',
+			'M0,-10 l-19,-10    h  -1 ' +
+			'a 3,3 0 0,1 -3, -3 v -28 ' +
+			'a 3,3 0 0,1  3, -3 h  40 ' +
+			'a 3,3 0 0,1  3,  3 v  28 ' +
+			'a 3,3 0 0,1 -3,  3 h  -1 z');
+		hintBG = hint.cloneNode();
+		hintBG.classList.add('hintWhiteBG');
+		hint.classList.add('hint');
+
+		spot.appendChild(circle);
+		spot.appendChild(hintBG);
+		spot.appendChild(hint);
+		spot.appendChild(hintTextX);
+		spot.appendChild(hintText);
+
+		this.spot = spot;
+	},
+	initVLineElements: function () {
+		var vLineEl = document.createElementNS(this.svgNS, 'g'),
+			vLinePath = document.createElementNS(this.svgNS, 'path'),
+			vLineTextEl = document.createElementNS(this.svgNS, 'g'),
+			vLineTextBG = document.createElementNS(this.svgNS, 'path'),
+			vLineText = document.createElementNS(this.svgNS, 'text');
+
+		vLinePath.classList.add('vLine');
+		vLineTextEl.setAttribute('transform', 'translate(0,-7)');
+
+		vLineTextBG.classList.add('vLineBG');
+		vLineTextBG.setAttribute('d', 'M0,0 V-15');
+
+		vLineText.setAttribute('y', -4);
+
+		vLineTextEl.appendChild(vLineTextBG);
+		vLineTextEl.appendChild(vLineText);
+		vLineEl.appendChild(vLinePath);
+		vLineEl.appendChild(vLineTextEl);
+
+		this.vLineEl = vLineEl;
+	},
+	generateChart: function (chartConfig) {
+		var j, k,
+			currentMainLine = this.mainLine.cloneNode(),
+			currentLinearGradient = this.linearGradient.cloneNode(true),
+			lineCoords = 'M',
+			currentSpot, currentVLine,
+			styleEl = this.containerEl.querySelector('style');
+
 		currentMainLine.style.stroke = chartConfig.color;
 		currentMainLine.classList.add(chartConfig.id);
 		// append line
-		containerEl.appendChild(currentMainLine);
+		this.gMain.appendChild(currentMainLine);
 
 		// defs
-		var currentLinearGradient = linearGradient.cloneNode(true);
 		currentLinearGradient.querySelector('.gradientColor').setAttribute('stop-color', chartConfig.color);
 		currentLinearGradient.setAttribute('id', chartConfig.id + '_gradient');
-		defs.appendChild(currentLinearGradient);
+		this.defs.appendChild(currentLinearGradient);
 
 		// iterate
-		var lineCoords = 'M';
-		var currentSpot;
-		for(j = 0, k = chartConfig.points.length; j < k; j++) {
-			x = singleStepX * (chartConfig.points[j].x - config.axisX.points[0]);
-			y =  config.height - stepY * chartConfig.points[j].y;
+		for (j = 0, k = chartConfig.points.length; j < k; j++) {
+			x = this.singleStepX * (chartConfig.points[j].x - this.config.axisX.points[0]);
+			y = this.config.height - this.stepY * chartConfig.points[j].y;
 
 			lineCoords += x + ',' + y + ' ';
 
-			currentSpot = spot.cloneNode(true);
+			currentSpot = this.spot.cloneNode(true);
 			currentSpot.classList.add(chartConfig.id);
 			currentSpot.setAttribute('transform', 'translate(' + x + ',' + y + ')');
 			currentSpot.style.stroke = chartConfig.color;
 			currentSpot.querySelector('.hint').style.fill = 'url(#' + chartConfig.id + '_gradient)';
 			currentSpot.querySelector('.yValue').textContent = chartConfig.points[j].y;
 			currentSpot.querySelector('.percentageValue').textContent = chartConfig.points[j].percent + '%';
-			spotsEl.appendChild(currentSpot);
+			this.spotsEl.appendChild(currentSpot);
 			//label here
 
-			var currentVLine = vLineEl.cloneNode(true);
+			currentVLine = this.vLineEl.cloneNode(true);
 			currentVLine.classList.add(chartConfig.id);
 			currentVLine.setAttribute('transform', 'translate(' + x + ',0)');
-			currentVLine.querySelector('.vLine').setAttribute('d', 'M0,0 V-' + (config.height - y - 10));
+			currentVLine.querySelector('.vLine').setAttribute('d', 'M0,0 V-' + (this.config.height - y - 10));
 			currentVLine.querySelector('text').textContent = chartConfig.points[j].x;
-			vLines.appendChild(currentVLine);
+			this.vLines.appendChild(currentVLine);
 		}
 		currentMainLine.setAttribute('d', lineCoords);
 
-		var styleEl = document.querySelector(container + ' style');
-		if(!styleEl) {
+		if (!styleEl) {
 			styleEl = document.createElement('style');
-			document.querySelector(container).appendChild(styleEl);
+			this.containerEl.appendChild(styleEl);
 		}
 		styleEl.innerHTML += '.showChart_' + chartConfig.id + ' .' + chartConfig.id + ' {display: block}';
 
-		if(chartConfig.selected)
-			document.querySelector(container).classList.add('showChart_' + chartConfig.id);
-	};
+		if (chartConfig.selected)
+			this.selectChart(chartConfig.id);
+	},
+	selectChart: function (id) {
+		id = id || 'none';
+		var currentClassList = this.containerEl.classList;
 
-	var selectedChart = 0; //for first init (e.g. by URL)
+		var toRemove = [], i;
+		for(i in currentClassList)
+			if(currentClassList.hasOwnProperty(i) && !isNaN(i))
+				if(currentClassList[i].match(/showChart_(\S)+/))
+					toRemove.push(currentClassList[i]);
+		if(toRemove)
+			for(i = 0; i < toRemove.length; i++)
+				currentClassList.remove(toRemove[i]);
 
-	for(i = 0, l = config.data.length; i < l; i++) {
-		if(i == selectedChart) {
-			config.data[i].selected = true;
-		}
-		this.generateChart(config.data[i]);
+		this.containerEl.classList.add('showChart_' + id);
+		if(document.querySelector('.current'))
+			document.querySelector('.current').classList.remove('current');
+		if(document.querySelector('#' + id))
+			document.querySelector('#' + id).classList.add('current');
+	},
+    onclickNav: function (e) {
+		if (e.target.tagName == 'SPAN')
+			this.selectChart(e.target.parentNode.id);
 	}
-	containerEl.appendChild(spotsEl);
 };
